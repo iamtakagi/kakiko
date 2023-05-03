2d90fb2ed02ac179b80b4c0f58a9c12aa9910787
iamtakagi <github@iamtakagi.net>
2023-05-03 18:41:16 +0900

    loading プロパティ追加

--- hooks/useFetchMessages.ts
+++ hooks/useFetchMessages.ts
@@ -4,20 +4,24 @@ import { useEffect, useState } from "react";
 
 interface Hooks {
   messages: Message[];
+  loading: boolean;
 }
 
 export const useFetchMessages = (): Hooks => {
   const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     (async () => {
       const response = await fetch('/api/messages');
       const data: Message[] = await response.json();
-      setMessages(data)
+      setMessages(data);
+      setLoading(false);
     })();
   }, []);
 
   return {
-    messages
+    messages,
+    loading
   };
 };
