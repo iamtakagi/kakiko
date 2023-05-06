7d2ce5a2b069c634a8113dbded401c8ef291dd59
iamtakagi <github@iamtakagi.net>
2023-05-06 16:06:39 +0900

    SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { MessageForm } from '@/components/message-form';
 import { MessageTimeline } from '@/components/message-timeline';
 import RefreshButton from '@/components/refresh-button';
+import { Seo } from '@/components/seo';
 
 const IndexPage: React.FC = () => {
   return (
     <div>
+      <Seo title={'kakiko'} description={'最強の無料掲示板'} />
       <MessageForm />
       <RefreshButton />
       <MessageTimeline />
