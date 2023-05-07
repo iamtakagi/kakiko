114ebe47a97a57ee9df7a38c7662cc15ea42b208
iamtakagi <github@iamtakagi.net>
2023-05-08 04:52:25 +0900

    metadata 追加

--- app/messages/[id]/page.tsx
+++ app/messages/[id]/page.tsx
@@ -1,5 +1,32 @@
 import Link from 'next/link';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 import { Message } from './message.client';
+
+export const dynamic = 'force-dynamic';
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const { id } = params;
+  const message = await findMessageById(id);
+  if (!message) return notFound();
+
+  return {
+    title: 'kakiko',
+    description: message.text,
+    openGraph: {
+      type: 'website',
+      siteName: 'kakiko',
+      title: 'kakiko',
+      description: message.text,
+      url: 'https:///kakiko.vercel.app/',
+    },
+    twitter: {
+      card: 'summary',
+      title: `kakiko`,
+    },
+  };
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
