5d4dba11057edac3810d6c738bdce602c75304e6
iamtakagi <github@iamtakagi.net>
2023-05-07 13:28:13 +0900

    switch にする

--- pages/api/messages.ts
+++ pages/api/messages.ts
@@ -5,26 +5,29 @@ import { selectMessages } from '@/lib/messages/selectMessages';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('X-Robots-Tag', 'noindex');
-
-  if (req.method === 'GET') {
-    if (req.query && req.query['id']) {
-      const { id } = req.query;
-      if (typeof id !== 'string') return;
-      const message = await findMessageById(id);
-      res.json(message);
-      return;
-    }
-
-    const data = await selectMessages();
-    res.json(data.rows);
-    return;
-  } else if (req.method === 'POST') {
-    if (!req.body) return;
-    const { text } = req.body;
-    if (typeof text !== 'string') return;
-    if (text.length > 30) return;
-    await insertMessage(text);
-    res.redirect(303, '/');
+  switch (req.method) {
+    case 'GET':
+      if (req.query && req.query['id']) {
+        const { id } = req.query;
+        if (typeof id !== 'string') return;
+        const message = await findMessageById(id);
+        res.json(message);
+        break;
+      }
+      const data = await selectMessages();
+      res.json(data.rows);
+      break;
+    case 'POST':
+      if (!req.body) return;
+      const { text } = req.body;
+      if (typeof text !== 'string') return;
+      if (text.length > 30) return;
+      await insertMessage(text);
+      res.redirect(303, '/');
+      break;
+    default:
+      res.status(405).end();
+      break;
   }
 };
 
