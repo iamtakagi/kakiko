d17b3be6011c8f779a23f6a8a6c7a2dd761577b3
iamtakagi <github@iamtakagi.net>
2023-05-08 05:54:38 +0900

    萌えカウンタ

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import { MessageForm } from '@/components/message-form';
 import { MessageTimeline } from '@/components/message-timeline';
+import { MoeCounter } from '@/components/moe-counter';
 import RefreshButton from '@/components/refresh-button';
 
 export default async function Page() {
   return (
     <div>
+      <MoeCounter />
       <MessageForm />
       <RefreshButton />
       <MessageTimeline />
---
+++ components/moe-counter.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+export const MoeCounter: React.FC = () => {
+  // eslint-disable-next-line @next/next/no-img-element
+  return (<img src="https://moe-counter.yude.jp/get/@:kakiko?theme=moebooru" alt=":kakiko" />);
+};
