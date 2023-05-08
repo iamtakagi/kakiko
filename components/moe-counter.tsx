1b3135a795a97305f4f8ed9721f5cf0ccf8e0db6
iamtakagi <github@iamtakagi.net>
2023-05-08 16:48:14 +0900

    prettier

--- components/moe-counter.tsx
+++ components/moe-counter.tsx
@@ -2,5 +2,5 @@
 
 export const MoeCounter: React.FC = () => {
   // eslint-disable-next-line @next/next/no-img-element
-  return (<img src="https://moe-counter.yude.jp/get/@:kakiko?theme=moebooru" alt=":kakiko" />);
+  return <img src="https://moe-counter.yude.jp/get/@:kakiko?theme=moebooru" alt=":kakiko" />;
 };
