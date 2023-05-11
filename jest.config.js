3293e00122024629053b8ba87f1cef3971b03d3f
iamtakagi <github@iamtakagi.net>
2023-05-12 06:17:51 +0900

    テスト時に .env.local を読み込ませる

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ['<rootDir>'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/tests/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
