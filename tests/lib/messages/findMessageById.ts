0878b74641471983b6ebc95dbbe214226580dfc6
iamtakagi <github@iamtakagi.net>
2023-05-12 06:30:18 +0900

    it 関数ではなく test 関数を使う

--- tests/lib/messages/findMessageById.ts
+++ tests/lib/messages/findMessageById.ts
@@ -2,7 +2,7 @@ import { findMessageById } from "../../../lib/messages/findMessageById";
 
 describe('lib/messages/findMessageById.ts', () => {
 
-    it('データベースからメッセージを取得できる', async () => {
+    test('データベースからメッセージを取得できる', async () => {
         const message = await findMessageById('1');
         expect(message).toBeDefined();
     });
