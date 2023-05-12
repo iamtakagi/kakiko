ec2b956ddc85a3297cc6c080ff1a8d162cb4947f
iamtakagi <github@iamtakagi.net>
2023-05-13 05:06:30 +0900

    コメント消す

--- stories/MessageForm.stories.tsx
+++ stories/MessageForm.stories.tsx
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { MessageForm } from '../components/MessageForm';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof MessageForm> = {
   title: 'Example/MessageForm',
   component: MessageForm,
